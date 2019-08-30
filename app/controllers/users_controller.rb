4349671077650c31c250e9d75b0160b19aca520c
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-08-30 15:01:20 -0500

    Create the new method, create and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+    @users = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :cellphone, :address)
+  end
 end
