0ef4c92f4de5d7420ad58daaf227fee4af4f26c1
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-05 16:33:05 -0500

    test the product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(build(:product)).to be_valid
+  end
+
+  context "validate the presence of the parameters" do
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:description) }
+
+    it { should validate_presence_of(:quantity) }
+
+    it { should validate_presence_of(:price) }
+  end
 end
