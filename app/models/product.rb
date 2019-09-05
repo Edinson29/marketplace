393404737f1800a39cfb491f2f9a8991dbf6626b
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-05 16:30:15 -0500

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
 end
