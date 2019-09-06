60c7f8b149ec22df9463dfd24063d0d31906c07c
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-06 15:18:27 -0500

    add the index test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe ProductsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "assigns an instance @users with a index User" do
+      get :index
+      expect(controller.instance_variable_get(:@products)).to eq(Product.order("id ASC"))
+    end
+
+    it "renders with index view" do
+      get :index
+      expect(response).to render_template("products/index")
+    end
+  end
 end
