f5019f1c2a7edd412c9c3225a287a17056f7333a
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-09 09:41:42 -0500

    create the create and new method and strong params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,19 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order("id ASC")
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save ? redirect_to @product : render 'new'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :quantity, :price)
+  end
 end
