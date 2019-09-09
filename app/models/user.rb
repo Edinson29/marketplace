c3a227f0385fdd52c81774e5386d6c512e50bb11
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-09 18:13:48 -0500

    create the name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
