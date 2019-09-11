1dbbdeaa02a3bcc1f8bb42da89dfaf38f3973582
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-11 10:20:12 -0500

    Correct index test

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,18 +3,26 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+
+    before do
       get :index
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
 
-    it "assigns an instance @users with a index User" do
-      get :index
-      expect(controller.instance_variable_get(:@products)).to eq(Product.published)
+    it "validates that the published product is included" do
+      product = create(:product, status: 'published')
+      expect(controller.instance_variable_get(:@products)).to include(product)
+    end
+
+    it "validates that the unpublished product is not included" do
+      product = create(:product, status: 'unpublished')
+      expect(controller.instance_variable_get(:@products)).not_to include(product)
     end
 
     it "renders with index view" do
-      get :index
       expect(response).to render_template("products/index")
     end
   end
