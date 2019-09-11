986341bcf07e83bd1f6305de7db51e9247395059
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-11 11:20:33 -0500

    Create edit and update method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,16 +8,17 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def edit; end
+
   def create
     @product = Product.new(product_params)
     @product.save ? redirect_to(@product) : render('new')
   end
 
-  def show
-  end
+  def show; end
 
   def update
-
+    @product.update(product_params) ? redirect_to(@product) : render('edit')
   end
 
   private
