1dcf599fef08ec4bc6d25c6b7554c4605cf7b1c7
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-12 10:07:57 -0500

    Add the relationship to the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   enum status: %i[unpublished archived published]
   belongs_to :user
+  has_many_attached :images
   validates :name, presence: true
   validates :description, presence: true
   validates :quantity, presence: true
