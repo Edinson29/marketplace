74e1d428814fb01b4120a35357f82cefb91a1c25
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-13 10:56:49 -0500

    add images to the products with active_storage and the nested_attributes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :quantity, :price, :user_id, images: [])
+    params.require(:product).permit(:name, :description, :quantity, :price, :user_id, images_attributes: [ :id, :avatar, :_destroy,])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_144000) do
+ActiveRecord::Schema.define(version: 2019_09_12_163350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2019_09_12_144000) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "images", force: :cascade do |t|
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -59,5 +66,6 @@ ActiveRecord::Schema.define(version: 2019_09_12_144000) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "images", "products"
   add_foreign_key "products", "users"
 end
