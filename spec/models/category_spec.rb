9c0fd7a5c5d431e0f8cfd452f4da03d6b94fc352
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-16 10:12:19 -0500

    Change the create for build in the test of the category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it "is valid with valid attributes" do
-    expect(create(:category, name: 'primary')).to be_valid
+    expect(build(:category)).to be_valid
   end
 
   it { should validate_presence_of(:name) }
