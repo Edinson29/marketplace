a337648e13095def295f5678f275de247ebff01d
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-16 14:50:39 -0500

    I generate the category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, except: %i[index new create]
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save ? redirect_to(@category) : render('new')
+  end
+
+  def update
+    @category.update(category_params) ? redirect_to(@category) : render('edit')
+  end
+
+  def destroy
+    @category.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   resources :users
   resources :products
   root 'users#index'
