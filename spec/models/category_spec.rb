16f7389965c3ade2e3dea96da393fd2d544dafdd
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-16 09:42:34 -0500

    Add the test to the category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(create(:category, name: 'primary')).to be_valid
+  end
+
+  it { should validate_presence_of(:name) }
 end
