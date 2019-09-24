6d9e59418344bd9be3ac2dcd8913ae8b669b3bcc
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-24 10:15:46 -0500

    create the view and method for send email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
+  include 'products_controller'
   default from: 'example@email.com'
   layout 'mailer'
+  def published_product(product)
+    @product = product
+    if publish
+      User.all.each do |user|
+        mail(to: user.email, subject: 'New published product')
+      end
+    end
+  end
 end
