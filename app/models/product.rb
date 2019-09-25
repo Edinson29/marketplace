0f62699ada3fc43ce2526f49bcd416f9ed14a2b5
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-25 17:15:55 -0500

    create the collback and private methods for send eamils to all users

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,19 @@ class Product < ApplicationRecord
   validates :quantity, presence: true
   validates :price, presence: true
   accepts_nested_attributes_for :images, allow_destroy: true
+  after_update :send_email, if: :change_to_published
+
+  private
+
+  def change_to_published
+    proc { |product|
+      product.saved_change_to_attribute?('status', to: 'published')
+    }
+  end
+
+  def send_email
+    User.all.each do |user|
+      ProductMailer.published_product(user, @product).deliver_later
+    end
+  end
 end
