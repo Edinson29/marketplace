b55374ccb089550b226a66862fa80e636a166e2d
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-25 17:12:03 -0500

    reorganize the view and the mailer

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,2 +1,7 @@
 class ProductMailer < ApplicationMailer
+  def published_product(user,product)
+    @product = product
+    @user = user
+    mail(to: @user.email, subject: 'New published product')
+  end
 end
