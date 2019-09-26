7f1f970cb425ed16933fc8d35c095e26da48be7f
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-26 09:54:31 -0500

    Add more test for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -31,22 +31,47 @@ RSpec.describe Product, type: :model do
 
   it { should callback(:send_email).after(:save).if(:change_to_published) }
 
-  describe "send email" do
+  describe "send email or not send email" do
     before do
       @user = create(:user)
       @category = create(:category)
     end
 
-    it "sends email to the users" do
-      expect {
-        create(:product, status: 'published', user_id: @user.id, category_id: @category.id)
-      }.to change(ActionMailer::Base.deliveries, :count).by(User.all.length)
+    context "send email" do
+      it "when the state is published" do
+        expect {
+          create(:product, status: 'published', user_id: @user.id, category_id: @category.id)
+        }.to change(ActionMailer::Base.deliveries, :count).by(User.all.length)
+      end
+
+      it "when update the status to publish" do
+        product = create(:product, user_id: @user.id, category_id: @category.id)
+        expect {
+          product.published!
+        }.to change(ActionMailer::Base.deliveries, :count).by(User.all.length)
+      end
     end
 
-    it "is not send email to users" do
-      expect {
-        create(:product, status: 'unpublished', user_id: @user.id, category_id: @category.id)
-      }.to change(ActionMailer::Base.deliveries, :count).by(0)
+    context "Not send email" do
+      it "when the state is unpublished" do
+        expect {
+          create(:product, status: 'unpublished', user_id: @user.id, category_id: @category.id)
+        }.to change(ActionMailer::Base.deliveries, :count).by(0)
+      end
+
+      it "when the state is update to unpublished" do
+        product = create(:product, status: 'published', user_id: @user.id, category_id: @category.id)
+        expect {
+          product.unpublished!
+        }.to change(ActionMailer::Base.deliveries, :count).by(0)
+      end
+
+      it "when publish an already published product" do
+        product = create(:product, status: 'published', user_id: @user.id, category_id: @category.id)
+        expect {
+          product.published!
+        }.to change(ActionMailer::Base.deliveries, :count).by(0)
+      end
     end
   end
 end
