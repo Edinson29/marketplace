c1adab9d7b162b55d83371e0ec6e00255aabe49d
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-09-27 17:29:24 -0500

    Delete the unnecesary validations in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
   has_many :products
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
   def name
     "#{first_name} #{last_name}"
   end
