58cd3e609a07c34edf680849750352cdf03fda2f
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-10-01 16:40:16 -0500

    create the login with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: %i[google_oauth2 facebook]
   has_many :products
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -12,9 +12,16 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-    user
+  def self.from_omniauth(auth)
+    data = auth.info
+    User.where(email: data['email']).first
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
 end
